42162370413bafc0fd5cdd769317647ad37f4283
Linda Khalaf <khalaflb24@gmail.com>
2015-02-05 23:00:07 -0800

    after video 18, creep1 is registered to pool and had been set up as a "child" of the "world" that i created

---
+++ after video 18/js/screens/play.js
@@ -0,0 +1,32 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
+
+		me.levelDirector.loadLevel("level01");//loaded level
+
+		var player = me.pool.pull("player", 0, 420);//pulling player to the pool
+		me.game.world.addChild(player, 5);//adding the player to the world	
+
+		me.input.bindKey(me.input.KEY.D, "right");//this binds the right key
+		me.input.bindKey(me.input.KEY.A, "left");//this binds the left key
+		me.input.bindKey(me.input.KEY.F, "attack");
+		me.input.bindKey(me.input.KEY.W, "jump");
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+});
