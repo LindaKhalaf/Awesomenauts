131e219ea1bdf48df8b0420b071c0a3510fee4cd
Linda Khalaf <khalaflb24@gmail.com>
2015-03-03 10:50:35 -0800

    video 37 completed, the four lines of code specified in video have been imputed

---
+++ after video 37/js/entities/GameManager.js
@@ -0,0 +1,67 @@
+game.GameTimerManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date().getTime();
+		this.paused = false;
+		this.lastCreep = new Date().getTime();
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		this.now = new Date().getTime();
+		this.goldTimerCheck();
+		this.creepTimerCheck();
+
+		return true;
+//hai
+	},
+	goldTimerCheck: function(){
+		if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
+			game.data.gold += 1;
+			console.log("Current gold count:" + game.data.gold);
+		}
+	},
+
+	creepTimerCheck: function(){
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			this.lastCreep = this.now;
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creepe, 5);
+			var creepe2 = me.pool.pull("MyCreep", 100, 0, {});
+			me.game.world.addChild(creepe2, 5);
+		}
+	}
+
+});
+
+game.HeroDeathManager = Object.extend({
+	init: function(x, y, settings){
+	this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		if(game.data.player.dead){
+			me.game.world.removeChild(game.data.player);
+			me.state.current().resetPlayer(10, 0);
+		}
+			return true;
+	},
+});
+
+game.ExperienceManager = Object.extend({
+	init: function(x, y, settings){
+		this.alwaysUpdate = true;
+		this.gameOver = false;
+	},
+	update: function(){
+		if(game.data.win === true && !this.gameOver){
+			game.data.exp += 10;
+			this.gameOver = true;
+		} else if(game.data.win === false && !this.gameOver){
+			game.data.exp += 1;
+			this.gameOver = true;
+		}
+		console.log(game.data.exp);
+		return true; 
+	}
+
+});
